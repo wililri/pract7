0f1fb0972520058957b6f12ee45c5d9e387932ea
Дилюк <@DESKTOP-5SJPVS1>
2024-04-18 13:41:08 +0300

    Добавьте файлы проекта.

---
+++ pract7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pract7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+
+                try
+                {
+                    StreamReader inFile = new StreamReader("F.txt");
+                    Console.SetIn(inFile);
+                    BinaryWriter F = new BinaryWriter(new FileStream("F.txt", FileMode.Create));
+                    Random random = new Random();
+
+                    for (int i = 0; i < 12; i++)
+                    {
+                        double valueWeather = random.Next(20);
+                        F.Write(valueWeather);
+
+                    }
+                    F.Close();
+
+                    BinaryReader Fread = new BinaryReader(new FileStream("F.txt", FileMode.Open));
+                    double[] monthlyTemperatures = new double[12];
+                    double sum = 0;
+
+                    for (int i = 0; i < 12; i++)
+                    {
+                        double temperature = Fread.ReadDouble();
+                        monthlyTemperatures[i] = temperature;
+                        sum += temperature;
+                        Console.WriteLine($"Месяц {i + 1}: {temperature}");
+                    }
+                    Fread.Close();
+
+
+                    StreamWriter outFile = new StreamWriter("G.txt");
+                    Console.SetOut(outFile);
+                    BinaryWriter G = new BinaryWriter(new FileStream("G.txt", FileMode.Create));
+                    Console.WriteLine("\nСодержимое файла G.txt (отклонения среднемесячной температуры от среднегодовой):");
+                    double averageYearlyTemperature = sum / 12;
+
+                    for (int i = 0; i < 12; i++)
+                    {
+                        double deviation = monthlyTemperatures[i] - averageYearlyTemperature;
+                        G.Write(deviation);
+
+                        Console.WriteLine($"Месяц {i + 1}: {deviation}");
+                    }
+                    G.Close();
+                }
+                catch (Exception ex) { }
+
+
+            }
+        }
+    }
+}
---
+++ pract7_1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const uint numberscount = 10;
+            try
+            {
+                BinaryWriter file = new BinaryWriter(new FileStream("binary.dat", FileMode.Create));
+                Random rand = new Random();
+                for (int i = 0; i < numberscount; i++)
+                {
+                    file.Write(rand.Next(100) + rand.Next(100) / 100.0d);
+                }
+                file.Close();
+                Console.WriteLine("Файл был заполнен:");
+
+            }
+            catch { }
+
+            double[] array = new double[10];
+            try
+            {
+                BinaryReader read = new BinaryReader(new FileStream("binary.dat", FileMode.Open, FileAccess.Read));
+                for (int i = 0; i < numberscount; i++)
+                {
+                    array[i] = read.ReadDouble();
+                    Console.WriteLine(array[i]);
+                }
+                read.Close();
+            }
+            catch { }
+
+
+            try
+            {
+                Console.WriteLine("Введите вещественное число:");
+                double input = double.Parse(Console.ReadLine());
+
+
+                BinaryWriter binaryWriter = new BinaryWriter(new FileStream("binary.dat", FileMode.Open, FileAccess.ReadWrite));
+                binaryWriter.Seek(0, SeekOrigin.End);
+
+
+
+                for (int i = 0; i < numberscount; i++)
+                {
+                    if (array[i] > input)
+                    {
+                        binaryWriter.Write(array[i]);
+                    }
+                }
+                binaryWriter.Close();
+
+
+                Console.WriteLine("Измененный файл: ");
+                BinaryReader binaryreader = new BinaryReader(new FileStream("binary.dat", FileMode.Open, FileAccess.Read));
+                try
+                {
+                    while (true)
+                    {
+                        Console.WriteLine(binaryreader.ReadDouble());
+                        Console.WriteLine("\t");
+                    }
+                }
+                catch { }
+                binaryreader.Close();
+
+            }
+            catch { }
+
+        }
+    }
+}
